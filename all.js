660c550338630bb66eba159bb29e6099c2384f8f
vxzaa7 <vxzaa7@gmail.com>
2022-06-02 23:42:54 +0800

    frist commit

--- all.js
+++ all.js
@@ -1,51 +1,32 @@
-const searchBtn = document.querySelector('nav .web-nav .link-search');
-const closeBtn = document.querySelector('.search-container .link-close');
-const webNav = document.querySelector('.web-nav');
-const searchBar = document.querySelector('.search-container');
+const desktopNav = document.querySelector('.desktop-nav__list');
+const desktopSearchBtn = document.querySelector(
+  '.desktop-nav__list .link-search',
+);
+
+const searchNav = document.querySelector('.main-search');
+const searchBtn = document.querySelector('.main-search .main-search__btn');
+const searchCloseBtn = document.querySelector(
+  '.main-search .main-search__close',
+);
 const overMask = document.querySelector('.over-mask');
 
-searchBtn.addEventListener('click', () =>{
-    webNav.classList.add("hide");
-    searchBar.classList.remove("hide");
-    overMask.classList.add("show")
-})
-
-closeBtn.addEventListener('click',()=>{
-    webNav.classList.remove("hide");
-    searchBar.classList.add("hide");
-    overMask.classList.remove("show")
-})
-
-overMask.addEventListener('click',()=>{
-    webNav.classList.remove("hide");
-    searchBar.classList.add("hide");
-    overMask.classList.remove("show")
-})
-
-
-
-//mobile
-const menuIcon = document.querySelector("nav .menu-icon-container");
-const navContainer = document.querySelector(".nav-container");
-
-menuIcon.addEventListener("click",()=>{
-    navContainer.classList.toggle("active");
-})
-
-
-const MobileSearch = document.querySelector('.mobile-search-container .search-bar');
-const searchInput = document.querySelector('.mobile-search-container input')
-const nav =  document.querySelector('.nav-container nav');
-const cancelBtn = document.querySelector('.mobile-search-container .cancel-btn')
-
-searchInput.addEventListener('click',()=>{
-    MobileSearch.classList.add('active');
-    nav.classList.add('move-up');
-    webNav.classList.add('move-down');
+//點擊桌面搜尋鍵
+desktopSearchBtn.addEventListener('click', () => {
+  desktopNav.classList.add('hide');
+  searchNav.classList.remove('hide');
+  overMask.classList.add('show');
 });
 
-cancelBtn.addEventListener('click',()=>{
-    MobileSearch.classList.remove('active');
-    nav.classList.remove('move-up');
-    webNav.classList.remove('move-down');
+//關閉搜尋列
+searchCloseBtn.addEventListener('click', () => {
+  desktopNav.classList.remove('hide');
+  searchNav.classList.add('hide');
+  overMask.classList.remove('show');
+});
+
+//點擊mask關閉搜尋列
+overMask.addEventListener('click', () => {
+  desktopNav.classList.remove('hide');
+  searchNav.classList.add('hide');
+  overMask.classList.remove('show');
 });
